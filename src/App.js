fd564afbdb36ff517852615391c440db9a5eb515
manas-shinde <manas.a.shinde11@gmail.com>
2023-06-02 09:48:19 +0530

    Updated the routing paths.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rental from "./components/rental";
 import NotFound from "./components/notFound";
+import NavBar from "./components/commons/navBar";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rental}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies"></Redirect>
-          <Redirect to="/not-found"></Redirect>
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Route path="/movies-list" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rental}></Route>
+            <Redirect from="/" exact to="/movies-list"></Redirect>
+            <Redirect to="/not-found"></Redirect>
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }

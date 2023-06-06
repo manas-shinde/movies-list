6893e68dc565f5e77dde7685b7b4239f56a65292
manas-shinde <manas.a.shinde11@gmail.com>
2023-06-06 23:00:33 +0530

    Added Route for new Login form.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import "./App.css";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rental from "./components/rental";
 import NotFound from "./components/notFound";
 import MoviesForm from "./components/moviesForm";
 import NavBar from "./components/commons/navBar";
+import LoginForm from "./components/loginForm";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -15,11 +16,12 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
-            <Route path="/not-found" component={NotFound}></Route>
+            <Route path="/login" component={LoginForm}></Route>
             <Route path="/movies-list" component={Movies}></Route>
             <Route path="/movies/:id" component={MoviesForm}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rental}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
             <Redirect from="/" exact to="/movies-list"></Redirect>
             <Redirect to="/not-found"></Redirect>
           </Switch>

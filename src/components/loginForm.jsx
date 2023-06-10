23691e39b505cb61f741370202c46bf441aebb4d
manas-shinde <manas.a.shinde11@gmail.com>
2023-06-10 15:46:42 +0530

    Extracted helper render methods.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,51 +1,31 @@
 import React from "react";
-import joi from "joi-browser";
-import Form from "./commons/form.jsx";
-
-import Input from "./commons/input.jsx";
+import Joi from "joi-browser";
+import Form from "./commons/form";
 
 class LoginForm extends Form {
-  // username = React.createRef();
-
   state = {
-    data: {
-      username: "",
-      password: "",
-    },
+    data: { username: "", password: "" },
     errors: {},
   };
+
   schema = {
-    username: joi.string().required().label("Username"),
-    password: joi.string().required().label("Password"),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
-  doSubmit() {
-    //Submit the form
-    console.log("Submitted!");
-  }
+  doSubmit = () => {
+    // Call the server
+    console.log("Submitted");
+  };
 
   render() {
     return (
       <div>
-        <h1>Login Form</h1>
-        <form onSubmit={this.onSubmit}>
-          <Input
-            name="username"
-            label="Username"
-            value={this.state.data.username}
-            error={this.state.errors.username}
-            onChange={this.onChange}
-          ></Input>
-          <Input
-            name="password"
-            label="Password"
-            value={this.state.data.password}
-            error={this.state.errors.password}
-            onChange={this.onChange}
-          ></Input>
-          <button disabled={this.validate()} className="btn btn-primary">
-            Login
-          </button>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );

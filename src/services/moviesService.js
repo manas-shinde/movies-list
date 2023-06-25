5f10b3ef08fd2138429d43ca07a9c95bef1ad887
manas-shinde <manas.a.shinde11@gmail.com>
2023-06-25 11:49:38 +0530

    Refactor the code.

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -1,16 +1,30 @@
 import axios from "axios";
 import config from "../config.json";
 
+const APIENDPOINT = config.apiUrl + "/movies";
+
+const movieUrl = (id) => {
+  return `${APIENDPOINT}/${id}`;
+};
+
 export function getMovies() {
-  return axios.get(config.apiUrl + "/movies");
+  return axios.get(APIENDPOINT);
 }
 
 export function deleteMovie(movieId) {
-  return axios.delete(`${config.apiUrl}/${movieId}`);
+  return axios.delete(movieUrl(movieId));
 }
 
 export function getMovie(movieId) {
-  return axios.get(`${config.apiUrl}/movies/${movieId}`);
+  return axios.get(movieUrl(movieId));
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+
+    return axios.put(movieUrl(movie._id), body);
+  }
+  return axios.post(`${APIENDPOINT}`, movie);
+}
